46b3805e256f8e0083f8af974918dd42d32721e3
Katsiaryna Klindziuk <supertramp@tut.by>
2022-02-23 21:52:08 +0300

    module2-task1: add two functions about getting rundom numbers and checking the lenght of the string

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const getRundomNumber = (numberOne, numberTwo) => {
+  if(numberOne > numberTwo) {
+    numberOne = Math.floor(numberOne);
+    numberTwo = Math.ceil(numberTwo);
+    return Math.floor(Math.random() * (numberOne - numberTwo)) + numberTwo;
+  }
+
+  numberOne = Math.ceil(numberOne);
+  numberTwo = Math.floor(numberTwo);
+  return Math.floor(Math.random() * (numberTwo - numberOne)) + numberOne;
+}
+
+const checkMaxLength = (string, maxLength) => {
+  return string.length <= maxLength ? true : false;
+}
+
+getRundomNumber(10, 5)
+checkMaxLength("Here is the short string", 50)
