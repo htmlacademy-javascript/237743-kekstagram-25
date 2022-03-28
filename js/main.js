42d59d8e2e0677b00c16c29be2b47a256c82c725
Katsiaryna Klindziuk <supertramp@tut.by>
2022-03-28 21:35:43 +0300

    Refactoring

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-import './miniature.js';
+import './miniatures.js';
--- js/miniatures.js
+++ js/miniatures.js
@@ -1,16 +1,14 @@
 import {getListPhotosDescriptions} from './data.js';
 
 const picturesContainer = document.querySelector('.pictures');
-const picturesTitle = document.querySelector('.pictures__title');
-picturesTitle.classList.remove('visually-hidden');
 
 const miniatureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const ListPhotosDescriptions = getListPhotosDescriptions();
+const listPhotos = getListPhotosDescriptions();
 
 const similarListFragment = document.createDocumentFragment();
 
-ListPhotosDescriptions.forEach(({url, likes, comments}) => {
+listPhotos.forEach(({url, likes, comments}) => {
   const miniatureElement = miniatureTemplate.cloneNode(true);
   miniatureElement.querySelector('img').src = url;
   miniatureElement.querySelector('.picture__likes').textContent = likes;
