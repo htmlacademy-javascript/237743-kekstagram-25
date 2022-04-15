a3869f99cb298a83076fa0f17cb5849d165e47b3
Katsiaryna Klindziuk <supertramp@tut.by>
2022-04-15 16:38:48 +0300

    Add possibility to chose and apply differets effects to the photo

---
+++ js/effect-photo.js
@@ -0,0 +1,127 @@
+const effectsList = document.querySelector('.effects__list');
+const photoUploadPreview = document.querySelector('.img-upload__preview img');
+const effectLevelSlider = document.querySelector('.effect-level__slider');
+const effectLevelValue = document.querySelector('.effect-level__value');
+
+noUiSlider.create(effectLevelSlider, {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  start: 1,
+  step: 0.1,
+  connect: 'lower',
+  format: {
+    to: (value) => {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return value.toFixed(1);
+    },
+    from: (value) => (
+      parseFloat(value)
+    ),
+  }
+});
+
+const LIST_PHOTO_EFFECT = {
+  'effect-none': {
+    filter: 'none',
+    unit: '',
+    class: '',
+    noUiSlider: {
+      range: {
+        min: 0,
+        max: 0
+      },
+      start: 0,
+      step: 0,
+    }
+  },
+  'effect-chrome': {
+    filter: 'grayscale',
+    unit: '',
+    class: 'chrome',
+    noUiSlider: {
+      range: {
+        min: 0,
+        max: 1
+      },
+      start: 1,
+      step: 0.1,
+    }
+  },
+  'effect-sepia': {
+    filter: 'sepia',
+    unit: '',
+    class: 'sepia',
+    noUiSlider: {
+      range: {
+        min: 0,
+        max: 1
+      },
+      start: 1,
+      step: 0.1,
+    }
+  },
+  'effect-marvin': {
+    filter: 'invert',
+    unit: '%',
+    class: 'marvin',
+    noUiSlider: {
+      range: {
+        min: 0,
+        max: 100
+      },
+      start: 100,
+      step: 1,
+    }
+  },
+  'effect-phobos': {
+    filter: 'blur',
+    unit: 'px',
+    class: 'phobos',
+    noUiSlider: {
+      range: {
+        min: 0,
+        max: 3
+      },
+      start: 3,
+      step: 0.1,
+    }
+  },
+  'effect-heat': {
+    filter: 'brightness',
+    unit: '',
+    class: 'heat',
+    noUiSlider: {
+      range: {
+        min: 1,
+        max: 3
+      },
+      start: 3,
+      step: 0.1,
+    }
+  }
+};
+
+const effectsChangeHandler = (evt) => {
+  photoUploadPreview.classList = '';
+  if (evt.target.id === 'effect-none') {
+    effectLevelSlider.classList.add('hidden');
+    photoUploadPreview.style.filter = 'none';
+  } else {
+    effectLevelSlider.classList.remove('hidden');
+    effectLevelSlider.noUiSlider.updateOptions(LIST_PHOTO_EFFECT[evt.target.id].noUiSlider);
+    photoUploadPreview.classList.add(`effects__preview--${LIST_PHOTO_EFFECT[evt.target.id].class}`);
+  }
+};
+
+effectLevelSlider.noUiSlider.on('update', () => {
+  const selectedEffect = effectsList.querySelector('input:checked').id;
+  console.log(effectLevelValue.value);
+  effectLevelValue.value = effectLevelSlider.noUiSlider.get();
+  photoUploadPreview.style.filter = `${LIST_PHOTO_EFFECT[selectedEffect].filter}(${effectLevelSlider.noUiSlider.get()}${LIST_PHOTO_EFFECT[selectedEffect].unit})`;
+});
+
+effectsList.addEventListener('change', effectsChangeHandler);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import './miniatures.js';
 import './form.js';
 import './scale-photo.js';
+import './effect-photo.js';
