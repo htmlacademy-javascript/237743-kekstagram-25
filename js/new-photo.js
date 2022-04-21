6556ea1256be1c78a23522ca8e8ed95e5a25d422
Katsiaryna Klindziuk <supertramp@tut.by>
2022-04-21 13:35:14 +0300

    Fix all comments about positions and names of constants

--- js/new-photo.js
+++ js/new-photo.js
@@ -1,4 +1,4 @@
-import { FILE_TYPES } from './constants.js';
+import { FILES_TYPES } from './constants.js';
 
 const fileChooser = document.querySelector('#upload-file');
 const photoPreview = document.querySelector('.img-upload__preview > img');
@@ -7,7 +7,7 @@ fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
 
-  const match = FILE_TYPES.some((it) => fileName.endsWith(it));
+  const match = FILES_TYPES.some((it) => fileName.endsWith(it));
 
   if(match) {
     photoPreview.src = URL.createObjectURL(file);
